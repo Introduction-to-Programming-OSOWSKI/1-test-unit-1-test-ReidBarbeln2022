06215b3576fb0975214afae01c1c886efb175a6f
ReidBarbeln2022 <814684@mystma.org>
2020-12-22 16:53:02 +0000

    test try 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def name(n):
+    return n
+print (name("Reid"))
+
+def bigAdd(x,y,z,a,b):
+    return x + y + z + a + b
+print (bigAdd(10, 20, 5, 2, 2))
+
+def fourOperations(x,y,z,a,b):
+    return x + y - ((z * a) / b)
+print (fourOperations(2, 5, 1, 9, 5))
